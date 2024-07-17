ccf743dbe1a5527ea83d75052896cd6f937ee559
João Chiroli <36668502+joaochiroli@users.noreply.github.com>
2024-07-17 09:18:57 -0300

    Add files via upload

---
+++ Projetos/script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get update -y
+sudo apt-get install htop -y
+sudo wget https://repo.zabbix.com/zabbix/7.0/debian/pool/main/z/zabbix-release/zabbix-release_7.0-2+debian12_all.deb
+sudo dpkg -i zabbix-release_7.0-2+debian12_all.deb
